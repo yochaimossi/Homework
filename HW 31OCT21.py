677d6932ef7b9b3e8eca7a3799fc24a3d2da3931
yochaimossi <89745656+yochaimossi@users.noreply.github.com>
2021-11-03 17:42:40 +0200

    Add files via upload

---
+++ HW 31OCT21.py
@@ -0,0 +1,14 @@
+### Q1
+
+fruits = ["apple","banana","cherry","kiwi","mango"]
+print ( [x for x in fruits if 'a' in x])
+
+### Q2
+
+some_list = [1,4,7,12,19,22,23,26]
+
+obj = [(i,"even" if i%2==0 else "odd") for i in some_list]
+print(obj)
+
+
+
